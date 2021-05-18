fc7249bcceb61e7cea7113bc73f69c37d16ed853
misuzu <watanabe_misuzu_0621@yahoo.co.jp>
2021-05-18 18:57:43 +0900

    購入履歴テーブル、購入明細テーブルにデータを保存する処理を追加 #11

--- www/model/cart.php
+++ www/model/cart.php
@@ -168,7 +168,7 @@ function delete_cart($db, $cart_id){
 }
 
 /**
- * カート商品チェックをし、カートデータを削除
+ * 商品購入
  * @param  obj   $db    DBハンドル
  * @param  array $carts
  * @return bool  false
@@ -179,21 +179,42 @@ function purchase_carts($db, $carts){
     // falseを返す
     return false;
   }
-  // $carts繰り返し
-  foreach($carts as $cart){
-    // 在庫数の更新が成功でないとき
-    if(update_item_stock(
-        $db, 
-        $cart['item_id'], 
-        $cart['stock'] - $cart['amount']
-      ) === false){
-      // セッション変数にエラーメッセージを追加
-      set_error($cart['name'] . 'の購入に失敗しました。');
+  try {
+    // トランザクション開始
+    $db->beginTransaction();
+    // 購入履歴登録
+    insert_orders($db, $carts[0]['user_id']);
+    // order_id取得
+    $order_id = $db->lastInsertId();
+    // $carts繰り返し
+    foreach($carts as $cart){
+      // 購入明細登録
+      insert_order_details($db, $order_id, $cart['item_id'], $cart['price'], $cart['amount']);
+      // 在庫数の更新が成功でないとき
+      if(update_item_stock(
+          $db, 
+          $cart['item_id'], 
+          $cart['stock'] - $cart['amount']
+        ) === false){
+        // セッション変数にエラーメッセージを追加
+        set_error($cart['name'] . 'の購入に失敗しました。');
+      }
     }
+    // カートデータ削除
+    delete_user_carts($db, $carts[0]['user_id']);
+    // コミット処理
+    $db->commit();
+    // trueを返す
+    return true;
+  // 例外発生時
+  } catch (PDOException $e) {
+    // ロールバック処理
+    $db->rollBack();
+    // セッション変数にエラーメッセージを追加
+    set_session($e->getMessage());
+    // falseを返す
+    return false;
   }
-  
-  // カートデータ削除
-  delete_user_carts($db, $carts[0]['user_id']);
 }
 
 /**
@@ -268,3 +289,42 @@ function validate_cart_purchase($carts){
   return true;
 }
 
+/**
+ * 購入履歴登録
+ * @param  obj   $db      DBハンドル
+ * @param  str   $user_id ユーザID
+ */
+function insert_orders($db, $user_id) {
+  // SQL文
+  $sql = "
+    INSERT INTO orders
+      (user_id)
+    VALUES
+      (?)
+  ";
+  // 入力値の配列
+  $params = array($user_id);
+  // SQL文を実行
+  execute_query($db, $sql, $params);
+}
+
+/**
+ * 購入明細登録
+ * @param  obj   $db      DBハンドル
+ * @param  int   $item_id 商品ID
+ * @param  int   $price   購入時の価格
+ * @param  int   $amount  購入数
+ */
+function insert_order_details($db, $order_id, $item_id, $price, $amount) {
+  // SQL文
+  $sql = "
+    INSERT INTO order_details
+      (order_id, item_id, price, amount)
+    VALUES
+      (?, ?, ?, ?)
+  ";
+  // 入力値の配列
+  $params = array($order_id, $item_id, $price, $amount);
+  // SQL文を実行
+  execute_query($db, $sql, $params);
+}
